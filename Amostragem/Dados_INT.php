bd02a771edada4a1b71e58dab7e359470924ece3
mSISTEm <82456715+mSISTEm@users.noreply.github.com>
2022-11-10 23:01:39 -0300

    Add files via upload

---
+++ Amostragem/Dados_INT.php
@@ -0,0 +1,194 @@
+<?php
+
+    session_start();
+    //error_reporting (0);
+    include_once('config.php');
+    //print_r($_SESSION);
+    if((!isset($_SESSION['e-mail_Defensor']) == true) and (!isset($_SESSION['Telefone_Def']) == true)){
+
+        unset($_SESSION['e-mail_Defensor']);
+        unset($_SESSION['Telefone_Def']);
+        header('Location: Home.php');
+
+    }$logado = $_SESSION['e-mail_Defensor'];
+
+    // if(!empty($_GET['search'])){
+
+    //     //$data = $_GET['search'];
+    //     $data = $_GET['search'];
+    //     $sql = "SELECT * FROM intérprete WHERE Doc_Interprete LIKE '$data' or Nome LIKE '%$data%' or Endereço_Int LIKE '%$data%' or Status_Int LIKE '$data%' or Contato_Int LIKE '%$data%' ORDER BY Nome ASC";
+    //     //echo $data;
+    //     //echo "<br>";
+    //     //echo "contem algo,pesquisar";
+        
+
+    // }   else{
+    //     //echo "vazio, trazer tudo";
+    //     $sql = "SELECT * FROM intérprete ORDER BY Nome ASC";
+    // }
+    $sql = "SELECT * FROM intérprete ORDER BY Nome ASC";
+    $result = $conexao->query($sql);
+
+    //print_r($result);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link  rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"  crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+    
+        <title>Dados Internos</title>
+        <style>
+            body{
+                font-family: Arial, Helvetica, sans-serif;
+                background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+                text-align: center;
+                color: white;
+            }  
+
+            .table-bg{
+                background-color: rgba(0,0,0,0.3)
+                border-radius: 15px 15px 0 0;
+            }
+
+            
+        </style>
+        </head>
+        <body>
+                <nav class="p-3 mb-2 bg-dark text-white">
+                    <nav class="p-3 mb-2 bg-info text-white">
+                        <div class="container-fluid">
+                        <img src="SIS-DPU.png" title>
+                        <img src="DPU.png" title>
+                        <a href="Sair.php">Finalizar Sessão</a>
+                        </div>
+                    </nav>
+                </nav>
+                <div class= "m-5 text-white table-bg">
+                    <table class="table" id="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Doc_Interprete</th>
+                            <th scope="col">Endereço_Int</th>
+                            <th scope="col">Contato_Int</th>
+                            <th scope="col">Telefone_int</th>
+                            <th scope="col">Status_Int</th>
+                            <th scope="col">Editar</th>
+
+                            </tr>
+                        </thead> 
+                        <tbody>
+
+                            <?php
+                                while($user_data = mysqli_fetch_assoc($result)){
+                                    echo "<tr>";
+                                    echo "<td>" .$user_data['Nome']."</td>";
+                                    echo "<td>" .$user_data['Doc_Interprete']."</td>";
+                                    echo "<td>" .$user_data['Endereço_Int']."</td>";
+                                    echo "<td>" .$user_data['Contato_Int']."</td>";
+                                    echo "<td>" .$user_data['Telefone_int']."</td>";
+                                    echo "<td>" .$user_data['Status_Int']."</td>";
+                                    echo "<td>
+                                        <a onclick='editardados()' class='btn btn-sm btn btn-primary' href='Edit_INT.php?Doc_Interprete=$user_data[Doc_Interprete]'>  
+                                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                                            </svg>
+                                        </a>
+                                        <a onclick='alterarestado()' class='btn btn-sm btn btn-danger' href='Delete_INT.php?Doc_Interprete=$user_data[Doc_Interprete]'>
+                                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+                                                <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>
+                                                <path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>
+                                            </svg>
+                                        </a>
+
+                                    </td>";
+                                    echo "</tr>";
+                                }
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+        </body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+        <script>
+            function alterarestado() {
+                alert("Estado do Intérprete(a) 0");
+            }
+            function editardados() {
+                alert("Sendo encaminhado para o local de edição dos dados");
+            }
+            
+            $(document).ready( function () {
+
+                $('#table thead tr')
+                    .clone(true)
+                    .addClass('filters')
+                    .appendTo('#table thead');
+
+                $('#table').DataTable({
+                    orderCellsTop: true,
+                    fixedHeader: true,
+                    initComplete: function () {
+                        var api = this.api();
+            
+                        // For each column
+                        api
+                            .columns()
+                            .eq(0)
+                            .each(function (colIdx) {
+                                // Set the header cell to contain the input element
+                                var cell = $('.filters th').eq(
+                                    $(api.column(colIdx).header()).index()
+                                );
+                                var title = $(cell).text();
+                                $(cell).html('<input type="text" autocomplete="off" placeholder="' + title + '" />');
+            
+                                // On every keypress in this input
+                                $(
+                                    'input',
+                                    $('.filters th').eq($(api.column(colIdx).header()).index())
+                                )
+                                    .off('keyup change')
+                                    .on('change', function (e) {
+                                        // Get the search value
+                                        $(this).attr('title', $(this).val());
+                                        var regexr = '({search})'; //$(this).parents('th').find('select').val();
+            
+                                        var cursorPosition = this.selectionStart;
+                                        // Search the column for that value
+                                        api
+                                            .column(colIdx)
+                                            .search(
+                                                this.value != ''
+                                                    ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                                    : '',
+                                                this.value != '',
+                                                this.value == ''
+                                            )
+                                            .draw();
+                                    })
+                                    .on('keyup', function (e) {
+                                        e.stopPropagation();
+            
+                                        $(this).trigger('change');
+                                        $(this)
+                                            .focus()[0]
+                                            .setSelectionRange(cursorPosition, cursorPosition);
+                                    });
+                            });
+                        },
+                });
+                
+            } );
+        </script>
+    </head>
+    </html> 
