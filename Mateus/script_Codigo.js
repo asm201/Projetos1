a1123d76ad4b8144ad290d4c771d4f4372a4f11d
mSISTEm <82456715+mSISTEm@users.noreply.github.com>
2022-11-26 23:09:21 -0300

    Add files via upload

--- Mateus/script_Codigo.js
+++ Mateus/script_Codigo.js
@@ -52,4 +52,41 @@ function Limpar(el) {
     var display = element.style.display;
     if (display != "none")
         element.style.display = 'none';
-}	
+}
+
+function RemoverOBG(el) {
+    let element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        element.required = false
+        element.value = null
+    } else {
+        const elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            element.required = false
+            element.value = null
+        }
+    }
+    var display = element.style.display;
+    if (display != "none")
+        element.style.display = 'none';
+
+}
+function ColocarOBG(el) {
+    let element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        element.required = true
+    } else {
+        let elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            element.required = true
+        }
+    }
+    var display = element.style.display;
+    if (display == "none")
+        element.style.display = 'block';
+
+}
