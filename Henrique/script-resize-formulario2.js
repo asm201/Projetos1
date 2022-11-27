55217b4776adf6d5c6ae3b354434c032f4bfbc8f
HSousaABC <112989025+HSousaABC@users.noreply.github.com>
2022-11-27 13:42:57 -0300

    Add files via upload

---
+++ Henrique/script-resize-formulario2.js
@@ -0,0 +1,20 @@
+<script>
+        function ResizeBorderDadoCrianca(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '735px';
+        }
+        function ResizeBorderCircunstancia(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '495px';
+        }
+        function ResizeBorderSituacao(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '917px';
+        }
+        function ResizeBorderMedidas(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '277px';
+        }
+        function ResizeBorderSiruacao2(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '438px';
+        }
+        function ResizeBorderInterprete(){
+            document.getElementById('Box_CadastroCrianca').style.marginTop = '245px';
+        }
+    </script>
