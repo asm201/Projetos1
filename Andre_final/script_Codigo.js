54b991efa4bc4db01c79e023ef6b6e6b4281e3ea
asm201 <41487739+asm201@users.noreply.github.com>
2022-11-28 12:51:46 -0300

    Add files via upload

---
+++ Andre_final/script_Codigo.js
@@ -0,0 +1,119 @@
+function Preencher(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none")
+        document.getElementById(el).style.display = 'block';
+}
+function Sumir(el) {
+    var display = document.getElementById(el).style.display;
+    if (display != "none")
+        document.getElementById(el).style.display = 'none';
+}
+
+function Validar(event, elements) {
+    for (let i = 0; i < elements.length; i++) {
+        const inputs = document.getElementById(elements[i]).querySelectorAll('input');
+        for (let j = 0; j < inputs.length; j++) {
+            if (!inputs[j].reportValidity()) {
+                event.preventDefault();
+                Swal.fire('Erro', 'Preencha a(s) etapa(s) anterior(es) completamente.', 'error').then(() => {
+                    inputs[j].reportValidity();
+                });
+                throw "Invalid";
+            }
+        }
+    }
+}
+
+function Requerido(el) {
+
+    const element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        if (element.required) {
+            element.value = null;
+        }
+    } else {
+        const elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            if (element.required) {
+                element.value = null;
+            }
+        }
+    }
+    var display = element.style.display;
+    if (display == "none")
+        element.style.display = 'block';
+}
+
+
+function Limpar(el) {
+    const element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        if (element.required) {
+            element.value = "N/A";
+        }
+    } else {
+        const elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            if (element.required) {
+                element.value = "N/A";
+            }
+        }
+    }
+    var display = element.style.display;
+    if (display != "none")
+        element.style.display = 'none';
+}
+
+function RemoverOBG(el) {
+    let element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        element.required = false;
+        element.value = null;
+    } else {
+        const elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            element.required = false;
+            element.value = null;
+        }
+    }
+    var display = element.style.display;
+    if (display != "none")
+        element.style.display = 'none';
+
+}
+function ColocarOBG(el) {
+    let element = document.getElementById(el);
+    //console.log(element.tagName);
+    if (element.tagName == "INPUT") {
+        element.required = true;
+    } else {
+        let elements = element.querySelectorAll('input');
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            element.required = true;
+        }
+    }
+    var display = element.style.display;
+    if (display == "none")
+        element.style.display = 'block';
+
+}
+
+function limparDatas() {
+    document.getElementById("Data_Chegada_Pessoa").value = '000-00-00';
+    document.getElementById("Data_Reconhecido_Pessoa").value = '000-00-00';
+  }
+
+function getDataSaida() {
+    document.getElementById("Data_Chegada_Pessoa").setAttribute("min", document.getElementById("Data_Saida_Pessoa").value);
+  }
+
+function getDataEntrada() {
+    document.getElementById("Data_Reconhecido_Pessoa").setAttribute("min", document.getElementById("Data_Chegada_Pessoa").value);
+}
